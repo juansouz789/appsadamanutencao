85868143f13342dce9ac46ace303c377efa02ddb
Juan Souza <juan.souza125@gmail.com>
2024-06-19 14:30:49 -0300

    .

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+
+
+function enviar(){
+    let sA = document.getElementById('nSA').value
+    let fluig = document.getElementById('nFlg').value
+    let data = document.getElementById('data').value
+
+    alert(`SA: ${sA}\n Fluig: ${fluig}\n Data de solicitação: ${data}`)
+}
