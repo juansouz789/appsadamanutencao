e744de2a4e433b02912243a86eb28e74f7181550
Juan Souza <juan.souza125@gmail.com>
2024-07-03 18:02:43 -0300

    atualizações

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,11 @@ function enviar(){
     let sA = document.getElementById('nSA').value
     let fluig = document.getElementById('nFlg').value
     let data = document.getElementById('data').value
+    let descricao = document.getElementById('desc').value
 
-    alert(`SA: ${sA}\n Fluig: ${fluig}\n Data de solicitação: ${data}`)
+    if (descricao === "" || data === "" || sA === "") {
+        alert("Os seguintes campos obrigatórios podem não estarem preenchidos:\nData, SA, Descrição. Favor revisar.")
+    }else{
+        alert(`SA: ${sA}\n Fluig: ${fluig}\n Data de solicitação: ${data}\n ${descricao}`)
+    }
 }
