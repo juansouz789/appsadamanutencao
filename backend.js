9949ec9cdec4b72ad5815df7c89a4202fd5f6735
Juan Souza <juan.souza125@gmail.com>
2024-07-12 14:30:41 -0300

    correção

--- backend.js
+++ backend.js
@@ -46,7 +46,7 @@ async function criarEpopularTabelaDeArmazem(sA, fluig, data, descricao) {
 }
 
 // para criar e popular a tabela
-app.post('http://192.168.30.85/api/criar-armazem', async (req, res) => {
+app.post('http://192.168.30.85:3000/api/criar-armazem', async (req, res) => {
     const { sA, fluig, data, descricao } = req.body;
 
     if (!sA || !data || !descricao) {
